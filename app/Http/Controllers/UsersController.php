91de84b2a73f06faf44f03ab89b75ec13741187d
langlibin <libin2010.lang@gmail.com>
2017-05-23 13:25:41 +0000

    Finish login and logot

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -27,7 +27,7 @@ class UsersController extends Controller
         $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
-            'password' => 'required'
+            'password' => 'required|confirmed|min:6'
         ]);
 
         $user = User::create([
@@ -36,6 +36,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
